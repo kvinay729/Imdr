3d057ab3af0397990cbb8105e519e0f4f80404ec
zopper <zopper@LP-B16>
2018-01-05 18:30:01 +0530

    New repo

---
+++ ImmediateRun/src/GFG.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GFG
+{
+  public static void main(String[] args)
+  {
+	  Scanner sc = new Scanner(System.in);
+	  System.out.println("Asking for length?");
+	  int l = sc.nextInt();
+	  int num[] = new int[l];
+	  for(int i=0;i<l;i++)
+	  {
+		  System.out.println(num[i]);
+	  }
+//		 int[] num = new int[l];
+//		 String s="vinay";
+//		 char arr[]=s.toCharArray();
+//		 char ar2[]= arr;
+//		 System.out.println("Enter"+" "+l+" "+"numbers");
+//		 for(int i=0;i<l;i++)
+		// {
+		// num[i]=sc.nextInt();
+////		  System.out.println(num[i]);
+//	  }
+//	  int  sr[] =num;
+//	  for(int i=0;i<l-1;i+=2)
+//	  {
+//		  System.out.println(sr[i]*2);
+//	  }
+//	  
+	  System.out.println("loop over");
+	  
+	  
+  }
+}
